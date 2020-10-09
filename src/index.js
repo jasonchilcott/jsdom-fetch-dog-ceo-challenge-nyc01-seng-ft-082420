07746f03279c5f9ad30ef2dca52f2fa20d628e1a
jasonchilcott <jasonchilcottcode@gmail.com>
2020-10-09 19:34:27 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 console.log('%c HI', 'color: firebrick')
+
+
+
+document.addEventListener("DOMContentLoaded", function(e){
+
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+    const renderDogs = dogs => {
+        for(const dog of dogs){
+            renderDog(dog)
+        }
+    }
+})
